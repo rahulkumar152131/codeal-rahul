83a7adb99c2566918296b1f3252f92e660f909f8
rahulkumar152131 <115453033+rahulkumar152131@users.noreply.github.com>
2023-07-29 15:37:15 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const env = require('./environment');
 mongoose.set("strictQuery", true);
-mongoose.connect(`mongodb://127.0.0.1/${env.db}`);
+mongoose.connect(`mongodb+srv://rk152531:8591@codeial.fhi4bjp.mongodb.net/?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 
